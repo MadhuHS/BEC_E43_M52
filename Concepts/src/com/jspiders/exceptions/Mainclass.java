21c40802b1a467be27397228c06eded79f576ebd
Madhuhs <madhuhs943@gmail.com>
2021-12-13 09:02:59 +0530

    Exceptions updated

--- Concepts/src/com/jspiders/exceptions/Mainclass.java
+++ Concepts/src/com/jspiders/exceptions/Mainclass.java
@@ -1,43 +1,43 @@
 package com.jspiders.exceptions;
 
-public class Mainclass 
-{
-	public static void test1()
-	{
+public class Mainclass {
+	public static void test1() {
 		System.out.println("this is test1()");
-		test2();//call test2()
+		test2();// call test2()
 	}
-	
-	public static void test2()
-	{
+
+	public static void test2() {
 		System.out.println("this is test2()");
-		test1();//call test1()
+		test1();// call test1()
 	}
-	
-	
-	public static void div(int n,int d)
-	{
+
+	public static void div(int n, int d) {
 		System.out.println("div() starts..");
-		
-		System.out.println("INPUTS : "+"n = "+n+" d = "+d);
-		
+
+		System.out.println("INPUTS : " + "n = " + n + " d = " + d);
+
 		int res = 0;
-		
-		res = n / d;
-		
-		System.out.println("OUTPUT : "+res);
-		
+
+		if (d > 0) 
+		{
+			res = n / d;
+			System.out.println("OUTPUT : " + res);
+		} 
+		else 
+		{
+			System.out.println("CANNOT DIVIDE BY ZERO");
+		}
+
 		System.out.println("div() ends..");
-		
-		return;//stop execution and exit from STACK
+
+		return;// stop execution and exit from STACK
 	}
-	
-	public static void main(String[] args)
-	{
-	   System.out.println("main() starts..");
-	   
-	   test1();
-	   
-	   System.out.println("main() ends..");
+
+	public static void main(String[] args) {
+		System.out.println("main() starts..");
+
+		div(10, 0);
+
+		System.out.println("main() ends..");
 	}
 }
